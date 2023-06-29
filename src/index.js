5ba16a6ab35fac588a66336931df61fa1d1d8067
Arthur <arthur.moreira@itec.ufpa.br>
2023-06-29 15:43:16 -0300

    06- middleware

--- src/index.js
+++ src/index.js
@@ -6,15 +6,26 @@ app.use(express.json());
 
 const customers = []
 
+// Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers
+
+  const customer = customer.find((customer) => customer.cpf === cpf)
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found!" })
+  }
+
+  req.customer = customer
+
+  return next()
+}
+
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body
   const customerAlreadyExists = customers.some(
     (customer) => customer.cpf === cpf
   )
-
-  if (customerAlreadyExists) {
-    return express.response.status(400).json({ error: "Customer already exists!" })
-  }
 
   customers.push({
     cpf,
@@ -24,6 +35,11 @@ app.post('/account', (req, res) => {
   })
 
   return res.status(201).send()
+})
+
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req
+  return res.json(customer.statement)
 })
 
 app.listen(3333);
